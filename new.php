b2acc07435dadb21b861202733b0c65073d8ba45
Luis Gerhorst <l.gerhorst@gmail.com>
2012-05-12 13:04:31 +0200

    htmlspecialchars() bug fix
    In some PHP Versions (especially 5.3.10) htmlspecialchars() (new.php
    line 8) seams to be a bit buggy, it adds a backslash [\] before single
    quotes ['] and double quotes ["], so I added two functions (new.php
    line 11 and 12) which remove it.

--- new.php
+++ new.php
@@ -1,11 +1,16 @@
 <?php
 
-// Die Formulardaten werden durch Javascript (Ajax) per POST and diese Datei geschickt, sie schreibt sie als HTML in chatlog.txt
+/* Die Formulardaten werden durch Javascript (Ajax) per POST and diese Datei geschickt, sie schreibt sie als HTML in chatlog.txt */
 
 $name = $_POST["name"]; // $name ist nun der per POST übergebene Name aus dem Formular
 
 $message = $_POST["message"]; // $content ist nun die per POST übergebene Nachricht aus dem Formular
-$message = strip_tags($message);
+$message = htmlspecialchars($message, ENT_QUOTES); // encodes special characters to HTML
+
+/* In some PHP Versions (especially 5.3.10) htmlspecialchars() seams to be a bit buggy, it adds a backslash [\] before single quotes ['] and double quotes ["], the following two functions remove it. */
+$message = ereg_replace("\&#039;", "&#039;", $message); // removes \ before '
+$message = ereg_replace("\&quot;", "&quot;", $message); // removes \ before "
+
 $message = ereg_replace("[[:alpha:]]+://[^<>[:space:]]+[[:alnum:]/]","<a href=\"\\0\">\\0</a>", $message); // macht Links (http://) anklickbar
 
 if ($message == "" or $name == "") // wenn nicht beide Felder ausgefüllt sind wird nichts gemacht
